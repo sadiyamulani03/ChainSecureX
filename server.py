66136f277f72082daadb09618769322b92ed29e7
Sadiya Mulani <sadiyamulani03@gmail.com>
2026-04-25 12:38:28 +0530

    Implement Diffie-Hellman key exchange in server
    Added Diffie-Hellman key exchange for enhanced security.

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 import socket
 import threading
 from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+
 from utils.crypto import encrypt_aes_key
+from utils.diffie_hellman import generate_private_key, generate_public_key, generate_shared_key
 
 HOST = '127.0.0.1'
 PORT = 5051
 
 clients = []
 
+# Shared AES key (Hybrid crypto)
 GLOBAL_AES_KEY = get_random_bytes(16)
 
 
@@ -24,14 +28,27 @@ def handle_client(conn, addr):
     print(f"Connected: {addr}")
 
     try:
-        from Crypto.PublicKey import RSA
-
+        # ---------------- RSA HANDSHAKE ---------------- #
         client_pub_key_data = conn.recv(4096)
         client_pub_key = RSA.import_key(client_pub_key_data)
 
         encrypted_aes_key = encrypt_aes_key(GLOBAL_AES_KEY, client_pub_key)
-
         conn.send(encrypted_aes_key)
+
+        # ---------------- DIFFIE-HELLMAN ---------------- #
+        server_private = generate_private_key()
+        server_public = generate_public_key(server_private)
+
+        # Send server public key
+        conn.send(str(server_public).encode())
+
+        # Receive client public key
+        client_public = int(conn.recv(1024).decode())
+
+        # Generate shared key
+        shared_key = generate_shared_key(client_public, server_private)
+
+        print(f"DH Shared Key (Server): {shared_key}")
 
     except Exception as e:
         print("Handshake Error:", e)
