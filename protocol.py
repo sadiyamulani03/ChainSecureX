08aa309e187675bbc8bb61444529a5087e75eddc
Sadiya Mulani <sadiyamulani03@gmail.com>
2026-04-30 19:03:19 +0530

    Update protocol.py

--- protocol.py
+++ protocol.py
@@ -1,10 +1,15 @@
 import json
+import base64
 
-def encode_message(username, message):
+def encode_message(username, message, msg_hash, signature):
     return json.dumps({
         "user": username,
-        "msg": message
+        "msg": message,
+        "hash": msg_hash,
+        "sig": base64.b64encode(signature).decode()
     }).encode()
 
 def decode_message(data):
-    return json.loads(data.decode())
+    decoded = json.loads(data.decode())
+    decoded["sig"] = base64.b64decode(decoded["sig"])
+    return decoded
